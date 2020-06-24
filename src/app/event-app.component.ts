4ca7d896632824105b6b4a73dfb53b63995ec540
shadhin.aust <shadhin.aust@gmail.com>
2020-06-24 21:34:36 +0600

    removed and refacyored codes

---
+++ src/app/event-app.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'event-app',
+  template: '<h2>Hello events</h2>',
+})
+export class EventAppComponent {
+  title = 'Event App';
+}
--- src/app/event-app.module.ts
+++ src/app/event-app.module.ts
@@ -1,16 +1,16 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { AppComponent } from './app.component';
+import { EventAppComponent } from './event-app.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    EventAppComponent
   ],
   imports: [
     BrowserModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [EventAppComponent]
 })
-export class AppModule { }
+export class EventAppModule { }
