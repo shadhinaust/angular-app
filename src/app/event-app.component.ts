9e869aaa83416fd5b2deef5a79bf09a0733c5138
shadhin.aust <shadhin.aust@gmail.com>
2020-06-24 22:12:15 +0600

    added bootstrap and static resources

--- src/app/event-app.component.ts
+++ src/app/event-app.component.ts
@@ -2,7 +2,8 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'event-app',
-  template: '<h2>Hello events</h2>',
+  template: `<h2>Hello events</h2>
+  <img src="/assets/images/basic-shield.png" />`,
 })
 export class EventAppComponent {
   title = 'Event App';
--- src/app/event-app.module.ts
+++ src/app/event-app.module.ts
@@ -2,15 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { EventAppComponent } from './event-app.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
+  imports: [
+    BrowserModule,
+    NgbModule
+  ],
   declarations: [
     EventAppComponent
   ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
   bootstrap: [EventAppComponent]
 })
 export class EventAppModule { }
