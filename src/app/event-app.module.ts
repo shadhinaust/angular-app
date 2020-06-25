eb629c9ff066913c1fffaca24f176b124b48c696
shadhin.aust <shadhin.aust@gmail.com>
2020-06-25 12:53:50 +0600

    added dom parsing

--- src/app/event-app.module.ts
+++ src/app/event-app.module.ts
@@ -5,6 +5,7 @@ import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { EventAppComponent } from './event-app.component';
 import { EventsListComponent } from './events/events-list.component';
 import { EventThumbnailComponent } from './events/event-thumbnail.component';
+import { NavBarComponent } from './nav/navbar.component';
 
 @NgModule({
   imports: [
@@ -12,6 +13,7 @@ import { EventThumbnailComponent } from './events/event-thumbnail.component';
     NgbModule
   ],
   declarations: [
+    NavBarComponent,
     EventAppComponent,
     EventsListComponent,
     EventThumbnailComponent
--- src/app/events/event-thumbnail.component.ts
+++ src/app/events/event-thumbnail.component.ts
@@ -2,13 +2,26 @@ import { Component, Input, Output, EventEmitter } from '@angular/core'
 
 @Component({
     selector: 'event-thumbnail',
-    templateUrl:'event-thumbnail.component.html'
+    templateUrl:'event-thumbnail.component.html',
+    styles:[`
+    .green { color: green !important;}
+    .blue { color: blue !important;}
+    .red { color: red !important;}
+    .bold {font-weight: bold;}
+    `]
 })
 export class EventThumbnailComponent{
     @Input() eventDetails:any
-    @Output() clickMe = new EventEmitter()
-    value: string = 'clicked event'
-    handleClickMe() {
-        this.clickMe.emit(this.value)
+
+    getStartTimeClass(){
+        if(this.eventDetails && this.eventDetails.time === '8:00 am'){
+            return ['green', 'bold']
+        }
+        else if(this.eventDetails && this.eventDetails.time === '10:00 am'){
+            return ['blue', 'bold']
+        }
+        else{
+            return 'red'
+        }
     }
 }
